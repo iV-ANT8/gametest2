e034cd86e48646d15aa1954044a5e04d6bb92e96
IvanVAntonyuk <110408121+iV-ANT8@users.noreply.github.com>
2025-06-18 23:20:26 -0300

    Update victoria.js Comentarios

--- tableroWeb/public/funciones/victoria.js
+++ tableroWeb/public/funciones/victoria.js
@@ -1,5 +1,7 @@
 import { globales } from "./globales.js";
 
+// FUNCIONES DE LA PESTAÑA DE VICTORIA AL LLEGAR A LOS 500 PUNTOS
+
 export function mostrarVictoria(){
   globales.popV.classList.remove("hidden");
 };
