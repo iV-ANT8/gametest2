a47f77ab76aca99404b66294c1c345a8a3951842
IvanVAntonyuk <110408121+iV-ANT8@users.noreply.github.com>
2025-06-18 22:46:14 -0300

    Update index.js console.log

--- MovimientosObservados/src/index.js
+++ MovimientosObservados/src/index.js
@@ -12,4 +12,6 @@ process.stdin.on("keypress", (_, key) => {
 process.stdin.setRawMode(true);
 process.stdin.resume();
 
-console.log("Usa la feclas para movertes y la q para salir");
+console.log("Usa la FLECHAS para moverte y la Q para salir");
+console.log("Usa la W para agregar un Observador Web y la E para quitarlo")
+console.log("Usa la C para agregar un Observador Consola y la V para quitarlo")
